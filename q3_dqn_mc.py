620f4293699811515294012332931fc1f398007a
Hang Gong <gohakido@gmail.com>
2018-03-06 16:11:44 -0500

    q3 MountainCar, fix memory bug.

--- q3_dqn_mc.py
+++ q3_dqn_mc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 import gym
+import random
 import sys, copy, argparse
 from collections import deque
 import keras
@@ -21,7 +22,10 @@ class QNetwork():
         # Define your network architecture here. It is also a good idea to define any training operations
         # and optimizers here, initialize your variables, or alternately compile your model here.
         self.model = Sequential()
-        self.model.add(Dense(num_actions, input_shape=feature_shape))
+        self.model.add(Dense(36, input_shape=feature_shape, activation='relu'))
+        self.model.add(Dense(48, activation='relu'))
+        # last layer should be linear not relu because we need both negative/positive values
+        self.model.add(Dense(num_actions))
         self.model.compile(optimizer=keras.optimizers.Adam(), loss=keras.losses.mse)
 
     def save_model_weights(self, weight_file):
@@ -36,31 +40,20 @@ class QNetwork():
         self.model.load_weights(weight_file)
 
 
-# class Replay_Memory():
-#
-#     def __init__(self, memory_size=50000, burn_in=10000):
-#         self.memory = deque(maxlen=memory_size)
-#         self.epsilon_min = 0.01
-#         self.epsilon_decay = 0.99
-#         self.epsilon = 1
-#         for state, action, reward, next_state, done in sample_batch(self)
-#             target_Q = reward
-#         if not done:
-#             target_Q = (reward + self.gamma * np.amax(self.model.predict(next_state)[0]))
-#         target_function = self.model.predict(state)
-#         target_function[0][action] = target_Q
-#         self.model.fit(state, target_function, epochs=1000, verbose=0)
-#         if self.epsilon > self.epsilon_min:
-#             self.epsilon *= self.epsilon_decay
-#
-#
-#     def sample_batch(self, batch_size=32):
-#         minibatch = random.sample(self.memory, batch_size)
-#         return minibatch
-#
-#
-#     def append(self, transition):
-#         self.memory.append((state, action, reward, next_state, done))
+class Replay_Memory():
+
+    def __init__(self, memory_size=50000, burnin_size=10000):
+        self.capacity = memory_size
+        self.burnin_size = burnin_size
+        self.memory = []
+
+    def sample_batch(self, batch_size=32):
+        return random.sample(self.memory, batch_size)
+
+    def append(self, *transaction):
+        if len(self.memory) == self.capacity:
+            self.memory = self.memory[1:]
+        self.memory.append(transaction)
 
 
 class DQN_Agent():
@@ -74,55 +67,65 @@ class DQN_Agent():
     # (4) Create a function to test the Q Network's performance on the environment.
     # (5) Create a function for Experience Replay.
 
-    def __init__(self, environment_name, render=False, gamma=.99,
-                 max_iteration=100000, max_episode=5000,
-                 max_eps=.5, min_eps=.05,
-                 mem_size=50000, batch_size=32, tfboard_enabled=False):
+    def __init__(self, environment_name, mem_size=50000, batch_size=32, tfboard_enabled=False):
         # Create an instance of the network itself, as well as the memory.
         # Here is also a good place to set environmental parameters,
         # as well as training parameters - number of episodes / iterations, etc.
+        self.envname = environment_name
         self.env = gym.make(environment_name)
         self.num_actions = self.env.action_space.n
         self.feature_shape = self.env.observation_space.shape
         self.qnet = QNetwork(self.feature_shape, self.num_actions)
-
-        self.max_iteration = max_iteration
-        self.max_episode = max_episode
-        self.gamma = gamma
-        self.max_eps = max_eps
-        self.min_eps = min_eps
+        self.model = self.qnet.model
+        self.max_iteration = 100000
+        self.max_episode = 5000
+        self.gamma = 1.
+        self.max_eps = 1.
+        self.min_eps = .05
         self.mem_size = mem_size
         self.batch_size = batch_size
         self.tfboard_enabled = tfboard_enabled
+        self.memory = Replay_Memory()
+
 
         # replace default with the recommended parameters........
         if environment_name == 'MountainCar-v0':
             self.max_episode = 5000
             self.gamma = 1.
-        elif environment_name == 'CartPole-v0':
-            self.max_episode = 1000000
-            self.gamma = .99
 
+
+
+    # This is the phi function described in the paper
+    def preprocess(self, state):
+        return state[None,:]
 
     # policy will return (a, q)
-    def epsilon_greedy_policy(self, q_values, eps):
-        # Creating epsilon greedy probabilities to sample from.
-        # TODO: check eps-greedy policy
+    def epsilon_greedy_policy(self, state, eps):
         if np.random.uniform() < eps:
-            a = self.env.action_space.sample()
-            return a, q_values[0][a]
+            return self.env.action_space.sample()
         else:
-            return self.greedy_policy(q_values)
+            return self.greedy_policy(state)
 
 
-    def greedy_policy(self, q_values):
+    def greedy_policy(self, state):
         # Creating greedy policy for test time.
-        a = q_values[0].argmax()
-        return a, q_values[0][a]
+        return np.argmax(self.model.predict(state))
 
 
     def eps_decay(self, i_iteration):
         return min(i_iteration, self.max_iteration) * (self.min_eps - self.max_eps) / self.max_iteration + self.max_eps
+
+
+    def train_batch(self):
+        model = self.model
+        batch = self.memory.sample_batch(self.batch_size)
+        states = np.zeros((self.batch_size, *self.feature_shape))
+        predict = np.zeros((self.batch_size, self.num_actions))
+        for i, (curr_state, action, reward, next_state, done) in enumerate(batch):
+            states[i] = curr_state
+            predict[i] = model.predict(curr_state)
+            predict[i][action] = reward if done else (reward + model.predict(next_state).max())
+        model.fit(states, predict, verbose=0)
 
 
     # policy: callable, which take in q_values and generate actions based on q_values
@@ -133,67 +136,49 @@ class DQN_Agent():
 
         # If you are using a replay memory, you should interact with environment here, and store these
         # transitions to memory, while also updating your model.
-        model = self.qnet.model
+        self.burn_in_memory()
+        model = self.model
         tensorboard = keras.callbacks.TensorBoard(log_dir='./log', histogram_freq=0, write_grads=False, write_images=False)
-        total_iteration = 0
 
+        # initialization
+        total_iteration = 0
+        total_reward = 0
+        all_reward = []
         for i_episode in range(self.max_episode):
-            # exceed the maximum iteration limit
-            if total_iteration > self.max_iteration:
-                break
 
             # reset the environment
             done = False
-            curr_state = self.env.reset()
+            curr_state = self.preprocess(self.env.reset())
             curr_iteration = 0
-            total_reward = 0
+            curr_reward = 0
 
             # TODO: gym takes care of max_episode_steps??？
             # if one episode reaches max steps, it will be automatically marked as 'done'
             while not done:
                 curr_iteration += 1
+                # perform eps decay
                 eps = self.eps_decay(curr_iteration + total_iteration)
 
-                # TODO: currently using batch_size = 1, change to mini-batch later
-                curr_state = curr_state[None,:]
+                action = self.epsilon_greedy_policy(curr_state, eps)
+                next_state, reward, done, _ = self.env.step(action)
+                next_state = self.preprocess(next_state)
 
-                # calculate current estimated q(s, a) and choose the greedy action
-                q_curr_target = model.predict(curr_state)
+                self.memory.append(curr_state, action, reward, next_state, done)
 
-                # this is the action a
-                curr_action, _ = self.epsilon_greedy_policy(q_curr_target, eps)
-
-                # take the action to the new state
-                # next_state: s', reward: r
-                next_state, reward, done, _ = self.env.step(curr_action)
-                total_reward += reward
-
-                if done:
-                    q_next = 0
-                else:
-                    # this is q(s', a')
-                    q_next_estimate = model.predict(next_state[None,:])
-
-                    # TODO: epsilon-greedy or greedy?
-                    _, q_next = self.greedy_policy(q_next_estimate)
-
-                # r + gamma * max(q(s', a'))
-                q_curr_target[0][curr_action] = reward + self.gamma * q_next
-
-                # q_curr_target = keras.utils.to_categorical([[q_curr_target]], self.num_actions)
-                # train it
-                if self.tfboard_enabled:
-                    model.fit(curr_state, q_curr_target, verbose=0, callbacks=[tensorboard])
-                else:
-                    model.fit(curr_state, q_curr_target, verbose=0)
+                self.train_batch()
                 # move to the next state
                 curr_state = next_state
+                curr_reward += reward
 
-            print('----- Episode {} done with {} steps, total reward={} -----'.format(i_episode, curr_iteration, total_reward))
             total_iteration += curr_iteration
-            if i_episode % 500 == 0:
-                model.save("./model/cartpole_dqn{}.h5".format(i_episode))
-
+            total_reward += curr_reward
+            all_reward.append(curr_reward)
+            avg40 = sum(all_reward[-40:]) / min(40, len(all_reward))
+            print('----- Episode {} done with {} steps, reward={}, avg40={:.4f}, avg={:.4f} -----'
+                  .format(i_episode, curr_iteration, curr_reward, avg40, total_reward/len(all_reward)))
+            if i_episode % 100 == 0:
+                model.save("./model/{}-e{}-avg40-{:.4f}.h5".format(self.envname, i_episode, avg40))
+        model.save("./model/{}-e{}-avg40-{:.4f}.h5".format(self.envname, self.max_episode, sum(all_reward[-40:]) / min(40, len(all_reward))))
 
     def test(self, model_file=None):
     # Evaluate the performance of your agent over 100 episodes, by calculating cummulative rewards for the 100 episodes.
@@ -222,24 +207,47 @@ class DQN_Agent():
                             curr_action, _ = self.greedy_policy(q_curr_estimate)
                             # take the action to the new state
                             # next_state: s', reward: r
-                            curr_state, reward, done, _ = self.env.step(curr_action)
+                            next_state, reward, done, _ = self.env.step(curr_action)
                             total_reward+=reward
                             if done:
                                     break
                     average_reward = total_reward/Test
                     #print ('episode: ',episode,'Evaluation Average Reward:',average_reward)
-                    if average_reward >= 300:
+                    if average_reward >= 200:
                             break
 
 
     def burn_in_memory(self):
         # Initialize your replay memory with a burn_in number of episodes / transitions.
-        pass
+        print('[burn-in] memory initialization start.')
+        episode = 0
+        while len(self.memory.memory) < self.memory.burnin_size:
+            # beginning of episode
+            # only burnin successful episode
+            done = False
+            transactions = []
+            curr_state = self.preprocess(self.env.reset())
+            total_reward = 0
+            episode += 1
+            print('[burn-in] episode={}'.format(episode))
+            while not done and len(self.memory.memory) < self.memory.burnin_size:
+                action = self.epsilon_greedy_policy(curr_state, self.max_eps)
+                next_state, reward, done, _ = self.env.step(action)
+                next_state = self.preprocess(next_state)
+                self.memory.append(curr_state, action, reward, next_state, done)
+                transactions.append((curr_state, action, reward, next_state, done))
+                curr_state = next_state
+                total_reward += reward
+            if total_reward > -200:
+                print('[burn-in] One episode with steps={} reward={} is burned in.'.format(len(transactions), total_reward))
+                # for tr in transactions:
+                #     self.memory.append(tr)
+        print('[burin-in] Done.')
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Deep Q Network Argument Parser')
-    parser.add_argument('--env', dest='env', type=str)
+    parser.add_argument('--env', dest='env', type=str, default='MountainCar-v0')
     parser.add_argument('--render', dest='render', type=int, default=0)
     parser.add_argument('--train', dest='train', type=int, default=1)
     parser.add_argument('--model', dest='model_file', type=str, default='')
@@ -257,7 +265,7 @@ def main():
 
     # Setting this as the default tensorflow session.
     keras.backend.tensorflow_backend.set_session(sess)
-    agent = DQN_Agent(environment_name)
+    agent = DQN_Agent('MountainCar-v0')
     agent.train()
 
 
